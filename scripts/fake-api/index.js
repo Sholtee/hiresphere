a6d7cef9b78d3f8a672baf3f2c58b70563ef7c4e
denes.solti <sodnaatx@gmail.com>
2025-05-24 08:50:52 +0200

    [fake-api] server & test data initials

---
+++ scripts/fake-api/index.js
@@ -0,0 +1,30 @@
+/*
+ * File: vite.config.js
+ * Project: job-ad
+ *
+ * Author: Denes Solti
+ */
+import {readFileSync, statSync} from 'node:fs';
+import {createServer} from 'https';
+
+import Database from 'better-sqlite3';
+import express from 'express';
+
+const
+  db = new Database('./fakeapi.db'),
+  app = express();
+
+if (!statSync(db.name).size) {
+  db.exec(readFileSync('./test-data.sql', {
+    encoding: 'utf8',
+    flag: 'r'
+  }));
+}
+
+app.get('/', (req, res) => {
+  res.setHeader('content-type', 'application/json');
+  res.status(200);
+  res.send(JSON.stringify('Hello world!'));
+});
+
+createServer({}, app).listen(1986);
