24da73c099ce8a5dfbdeca1557240391a68d8ffe
denes.solti <sodnaatx@gmail.com>
2025-05-30 07:58:38 +0200

    [e2e-tests] test theme

---
+++ tests/cypress/e2e/specs/theme.cy.js
@@ -0,0 +1,32 @@
+/*
+ * File: theme.cy.js
+ * Project: job-ad
+ *
+ * Author: Denes Solti
+ */
+
+/* eslint-disable no-undef */
+describe('Theme', () => {
+  it('can be dark', () => {
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        // Stub matchMedia to simulate dark mode
+        cy.stub(win, 'matchMedia').callsFake(query => ({
+          matches: query === '(prefers-color-scheme: dark)'
+        }));
+      }
+    });
+
+    cy
+      .get('#app')
+      .should('have.css', 'background-color')
+      .and('eq', 'rgb(44, 44, 44)');
+
+    cy.get('label[for="dark-mode"]').click();
+
+    cy
+      .get('#app')
+      .should('have.css', 'background-color')
+      .and('eq', 'rgb(250, 250, 250)');
+  });
+});
